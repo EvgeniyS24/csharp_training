a99c382cb9c49cf98225a360b6b81be30c0de16c
e.sergeev <e.sergeev@mts.ru>
2022-06-08 17:30:02 +0300

    Исправленная версия задания №2(пошагово работает, при запуске так же падает)

--- addressbook-web-tests/addressbook-web-tests/UnitTest2.cs
+++ addressbook-web-tests/addressbook-web-tests/UnitTest2.cs
@@ -64,26 +64,6 @@ namespace addressbook_web_tests
             driver.FindElement(By.Name("submit")).Click();
             driver.FindElement(By.LinkText("group page")).Click();
             driver.FindElement(By.LinkText("Logout")).Click();
-            driver.Navigate().GoToUrl("http://localhost/addressbook/group.php");
-            driver.FindElement(By.Name("user")).Click();
-            driver.FindElement(By.Name("user")).Clear();
-            driver.FindElement(By.Name("user")).SendKeys("admin");
-            driver.FindElement(By.Name("pass")).Click();
-            driver.FindElement(By.Name("pass")).Clear();
-            driver.FindElement(By.Name("pass")).SendKeys("secret");
-            driver.FindElement(By.XPath("//input[@value='Login']")).Click();
-            driver.FindElement(By.Name("new")).Click();
-            driver.FindElement(By.Name("group_name")).Click();
-            driver.FindElement(By.Name("group_name")).Clear();
-            driver.FindElement(By.Name("group_name")).SendKeys("a");
-            driver.FindElement(By.Name("group_header")).Click();
-            driver.FindElement(By.Name("group_header")).Clear();
-            driver.FindElement(By.Name("group_header")).SendKeys("a");
-            driver.FindElement(By.Name("group_footer")).Click();
-            driver.FindElement(By.Name("group_footer")).Clear();
-            driver.FindElement(By.Name("group_footer")).SendKeys("a");
-            driver.FindElement(By.Name("submit")).Click();
-            driver.FindElement(By.LinkText("group page")).Click();
         }
         private bool IsElementPresent(By by)
         {
